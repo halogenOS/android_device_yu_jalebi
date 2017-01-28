70bc262d39c2be8930dc164e7030d456ed475636
Harsh Shandilya <msfjarvis@gmail.com>
2017-01-29 03:21:08 +0530

    jalebi : remove duplicated and ignored configs
    Change-Id: I199add78095161c83eb633438f3bb25f84d51ba7

--- device.mk
+++ device.mk
@@ -26,8 +26,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    $(LOCAL_PATH)/configs/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
